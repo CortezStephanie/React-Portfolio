24b77ed5d48752d7c3692313f9f2c5a1501414cf
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-11-23 15:47:22 -0600

    validation for email added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Contact.css';
+import {validateEmail} from '../../utils/helpers';
+
+
 
 function Contact () {
+    const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleInputChange = (e) => {
+        const inputType= e.target.name;
+        const inputValue = e.target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email)){
+            setErrorMessage('Email is invalid');
+            return;
+        }
+        setEmail('');
+
+    }
     return (
         <div className='Container'>
             <h1 id="contactTitle">Contact Me</h1>
             <form id="formstuff">
-                <input placeholder="Full Name"></input>
-                <input type="Email" placeholder="Email"></input>
-                <textarea label="Message"></textarea>
+                <input placeholder="Full Name" className="inputs"></input>
+                <input value={email} onChange={handleInputChange} type="Email" placeholder="Email" className="inputs"></input>
+                <textarea label="Message" id="textA"></textarea>
+                <button type="button" onClick={handleFormSubmit}>Submit</button>
             </form>
+            {errorMessage && (
+                <div>
+                    <p className='error-text'>{errorMessage}</p>
+                    </div>
+            )}
         </div>
-    )  
+    );  
 }
 
 export default Contact;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../styles/Resume.css'
 
 export default function Resume () {
+
+    
     return (
         <div>
             <h1 id="resumeTitle">Resume</h1>
