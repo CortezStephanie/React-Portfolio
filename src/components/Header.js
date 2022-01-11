60ea9804469d97d4670a0130911fcd04d3b625b7
CortezStephanie <cortezstephanie2389@yahoo.com>
2022-01-11 14:28:06 -0600

    added image of myself

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {useState}  from 'react';
 import Navbar from './Navbar';
+import Intro from './Intro';
 import Footer from './Footer';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
@@ -62,8 +63,8 @@ export default function Header () {
     return (
         <div>
             <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
+            <Intro />
             {renderPage()}
-            
             <Footer/>
         </div>
     );
---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Intro.css';
+
+export default function Intro () {
+    return(
+        <>
+        <figure>
+            <img src='/images/me.jpg' alt='Image of Stephanie' className='picMe'></img>
+        </figure>
+        </>
+    )
+}
