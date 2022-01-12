ca2e62192366bc8579c9b1561752150fa79fa6f0
CortezStephanie <cortezstephanie2389@yahoo.com>
2022-01-12 10:16:40 -0600

    added image and name

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,9 +4,10 @@ import '../styles/Intro.css';
 export default function Intro () {
     return(
         <>
-        <figure>
-            <img src='/images/me.jpg' alt='Image of Stephanie' className='picMe'></img>
-        </figure>
+            <figure className='intro'>
+              <figcaption className='name'>Hi I'm Stephanie</figcaption>
+              <img src='/images/me.jpg' alt='Image of Stephanie' className='picMe'></img>
+            </figure>
         </>
     )
 }
