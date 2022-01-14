e27a4bb785d73924a4b3aa1cb589126ac059b2fd
CortezStephanie <cortezstephanie2389@yahoo.com>
2022-01-14 12:56:58 -0600

    added image in about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,15 +7,18 @@ function About () {
         <div>
             <h1 className="sectionTitle">About Me</h1>
             <section className="aboutContent">
-            <p>
-            Recent Full-Stack developer graduate from the University of Texas Austin, Full-Stack bootcamp. 
-            Certified in JavaScript, CSS, React.js, MERN stack, and responsive web design. My technical knowledge of 
-            cutting-edge web technologies, together with my problem-solving skills and dedication, make me a valuable 
-            member of any engineering team. I enjoy tackling programming problems from a variety of perspectives and 
-            cooperating with others. With my most recent project I worked on a team of 5 to develop a single-page MERN 
-            workbook that helps students learn to code. I am excited to immerse myself in this industry over the coming 
-            years as I continue to grow in this fast-paced world of development. 
-            </p>
+                <figure>
+                    <img src='/images/ben-kolde-FaPxZ88yZrw-unsplash.jpg' alt='unsplash photo' className='aboutPhoto'></img>
+                </figure>
+                <p>
+                    Recent Full-Stack developer graduate from the University of Texas Austin, Full-Stack bootcamp. 
+                    Certified in JavaScript, CSS, React.js, MERN stack, and responsive web design. My technical knowledge of 
+                    cutting-edge web technologies, together with my problem-solving skills and dedication, make me a valuable 
+                    member of any engineering team. I enjoy tackling programming problems from a variety of perspectives and 
+                    cooperating with others. With my most recent project I worked on a team of 5 to develop a single-page MERN 
+                    workbook that helps students learn to code. I am excited to immerse myself in this industry over the coming 
+                    years as I continue to grow in this fast-paced world of development. 
+                </p>
             </section>
         </div>
     )
